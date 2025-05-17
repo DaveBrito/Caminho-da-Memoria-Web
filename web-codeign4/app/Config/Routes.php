a11590fd05b204dce1e5ad92c055e833b28d5d2d
DaveBrito <davibritojunior1@gmail.com>
2025-05-17 11:09:20 -0300

    :recycle: refactor: Alteração de rota
    - Permite que seja acessível caminho de Perfil dentro de 'Index.php'

--- web-codeign4/app/Config/Routes.php
+++ web-codeign4/app/Config/Routes.php
@@ -6,8 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->group('', ['namespace' => 'App\Controllers'], function($routes) {
     $routes->get('/', 'Home::index');          // Página inicial
+    $routes->get('perfil', 'Home::perfil');
     $routes->get('memorias', 'Memorias::index');      // Página de memórias
     $routes->get('exercicios', 'Exercicios::index');  // Página de exercícios
-    $routes->get('perfil', 'Perfil::index');          // Página de perfil
+    //$routes->get('perfil', 'Perfil::index');          // Página de perfil
     $routes->get('login', 'Login::index');            // Página de login
+   
 });
